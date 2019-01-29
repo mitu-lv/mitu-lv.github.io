f7a2ff10d96e0a680c644b1fac21d54942530a33
Mikus Lacarus <mikus.lacarus@prezi.com>
2019-01-29 10:37:59 +0200

    Preparing playground.

--- js/desa.js
+++ js/desa.js
@@ -2,6 +2,21 @@
 var PLAYER = 1;
 var CPU = 2;
 
+function createSVG(paths) {
+    var namespace = 'http://www.w3.org/2000/svg';
+    var svg = document.createElementNS(namespace, 'svg');
+
+    svg.setAttribute('viewBox', '0 0 128 128');
+
+    paths.forEach(d => {
+        var path = document.createElementNS(namespace, 'path');
+        path.setAttribute('d', d);
+        svg.appendChild(path);
+    });
+
+    return svg;
+}
+
 function desa(container) {
     var pgState = [
         [0, 2, 0],
@@ -16,43 +31,62 @@ function desa(container) {
 
         pgState[row][col] = who;
 
-        drawPg();
+        updatePg(row, col, who);
     }
 
     function drawPg() {
-        var fc = container.firstChild;
-
-        while( fc ) {
-            container.removeChild( fc );
-            fc = container.firstChild;
-        }
-
         pgState.forEach(function(row, i) {
             drawPgRow(row, i);
         });
     }
 
+    function getStatStyle(who) {
+        var className = 'empty';
+
+        switch(who) {
+            case 1:
+                className += ' player';
+            break;
+            case 2:
+                className += ' cpu';
+            break;
+        }
+
+        return className;
+    }
+
+    function updatePg(row, col, who) {
+        const index = (3 * row + col);
+
+        var blockElem = document.querySelector('.empty:nth-child(' +  (index + 1) + ')');
+
+        blockElem.className = getStatStyle(who);
+    }
+
     function drawPgRow(row, rowIndex) {
         row.forEach(function(item, colIndex) {
-            var blockElem = this.blockElem = document.createElement('div');
-            var className = 'empty';
+            var blockElem = document.createElement('div');
+            var x = createSVG(['M16,16L112,112', 'M112,16L16,112']);
+            var o = createSVG(['M64,16A48,48 0 1,0 64,112A48,48 0 1,0 64,16']);
 
-            switch(item) {
-                case 1:
-                    className += ' player';
-                break;
-                case 2:
-                    className += ' cpu';
-                break;
-            }
+            x.setAttribute('class', 'xSprite');
+            x.querySelectorAll('path').forEach(d => {
+                d.setAttribute('class', 'xPath')
+            });
 
-            blockElem.className = className;
+            o.setAttribute('class', 'oSprite');
+            o.querySelector('path').setAttribute('class', 'oPath');
+
+            blockElem.appendChild(x);
+            blockElem.appendChild(o);
+
+            container.appendChild(blockElem);
+
+            blockElem.className = getStatStyle(pgState[rowIndex][colIndex]);
 
             blockElem.onclick = function() {
                 markState(rowIndex, colIndex, PLAYER);
             };
-
-            container.appendChild(blockElem);
         });
     }
 
